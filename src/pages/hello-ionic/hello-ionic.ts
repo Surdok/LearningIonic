908af36f7ab9af6596071fe37dca494eeaaefd0a
Surdok <ahmed_dolabi@hotmail.com>
2018-05-17 11:50:00 +0300

    Firebase something

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -4,73 +4,63 @@ import { NavController, NavParams } from 'ionic-angular';
 
 import firebase, { firestore } from 'firebase';
 import 'firebase/firestore';
+import { Title } from '@angular/platform-browser/src/browser/title';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  items: Array<{title: string, message: string}>;
+  items: Array<TodoItem>;
+  //items: Array<any>;
   Item = {};
-  
+
   //db = firebase.firestore();
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-let db = firebase.firestore();
 
-this.items = [];
-db.collection("Todo List").onSnapshot(function(querySnapshot){
-  querySnapshot.docChanges.forEach(function(change){
-                 if(change.type=="added"){
-                   console.log(change);
-                 }else if(change.type == "modified"){//modified
-
-                 }else if(change.type == "removed"){//removed
-
-
-                 }
- });
- console.log(this.items); // <--
-});
-
-  // db.collection("Todo List")
-  //   .get()
-  //   .then(function(querySnapshot) {
-  //       querySnapshot.forEach(function(doc) {
-  //         //this.items = [];
-  //           console.log(doc.id, " => ", doc.data());
-
-  //           // this.items.push({
-  //           //   title: doc.data().Title,
-  //           //   message: doc.data().Message
-  //           // });
-
-  //           // console.log(this.items);
-            
-  //       });
-
-  //   })
-  //   .catch(function(error) {
-  //       console.log("Error getting documents: ", error);
-  //   });
-
-    
-  }
-
-  AddItem(I){
-    console.log('Testing firebase..');
-    console.log(I);
     let db = firebase.firestore();
-    // Add a new document with a generated id.
-db.collection("Todo List").add({
-  Message: I.message,
-  Title: I.title
-})
-.then(function(docRef) {
-  console.log("Document written with ID: ", docRef.id);
-})
-.catch(function(error) {
-  console.error("Error adding document: ", error);
-});
+
+    this.items = [];
+
+    db.collection("Todo List")
+      .get()
+      .then((querySnapshot) => {
+        // querySnapshot.forEach((doc) => {
+        //   var _item = doc.data();
+        //   this.items.push(_item);
+        // });
+
+        var _items = querySnapshot.docs.map((value) => {
+          return {Title: value.data().Title, Message: value.data().Message};
+        });
+
+        this.items = _items;
+
+      })
+      .catch((error) => {
+        console.log("Error getting documents: ", error);
+      });
+
 
   }
+
+  AddItem(itemToAdd) {
+
+    let db = firebase.firestore();
+
+    db.collection("Todo List").add(itemToAdd)
+      .then((docRef) => {
+        this.items.push(itemToAdd);
+      })
+      .catch(function (error) {
+        console.error("Error adding document: ", error);
+      });
+
+  }
+}
+
+interface TodoItem {
+  Title: string;
+  Message: string;
 }
